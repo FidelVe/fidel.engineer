99cd7c8f9d3d69ec8d6dd50ceb46cf0af9fb396c
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-10-07 18:20:48 -0400

    refactoring

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,15 @@ export default function Contact() {
   return (
     <>
       <h2>Contact</h2>
+      <p>
+        I&apos;m always open to listen to any interesting project or job
+        proposal, if you want to get in contact with me you can send me an email
+        to{" "}
+        <a href="mailto:work@fidel.engineer">
+          <b>work @ fidel.engineer</b>
+        </a>
+        .
+      </p>
     </>
   );
 }
